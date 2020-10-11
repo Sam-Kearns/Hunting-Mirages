163818f74720bfc8bd472ed116c1c8295852e1f6
Sam <sam@kearns.net.au>
2020-10-11 21:15:26 +1100

    Bugfix missing styles in production by adding to whitelist from Tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,56 @@
 module.exports = {
-  purge: ['./components/**/*.js', './pages/**/*.js'],
-  theme: {
-    extend: {
-      colors: {
-        'accent-1': '#FAFAFA',
-        'accent-2': '#EAEAEA',
-        'accent-7': '#333',
-        success: '#0070f3',
-        cyan: '#79FFE1',
-      },
-      spacing: {
-        28: '7rem',
-      },
-      letterSpacing: {
-        tighter: '-.04em',
-      },
-      lineHeight: {
-        tight: 1.2,
-      },
-      fontSize: {
-        '5xl': '2.5rem',
-        '6xl': '2.75rem',
-        '7xl': '4.5rem',
-        '8xl': '6.25rem',
-      },
-      boxShadow: {
-        small: '0 5px 10px rgba(0, 0, 0, 0.12)',
-        medium: '0 8px 30px rgba(0, 0, 0, 0.12)',
-      },
+    purge: {
+        content: ['./components/**/*.js', './pages/**/*.js'],
+        options: {
+            whitelist: [
+                'xl:flex-row-reverse',
+                'pl-50pc',
+                'pr-50pc',
+                'w-25pc',
+                'w-50pc',
+                'text-right',
+                'text-left',
+            ]
+        }
     },
-    screens: {
-      'sm': '640px', // => @media (min-width: 640px) { ... }
-      'md': '768px', // => @media (min-width: 768px) { ... }
-      'lg': '1024px', // => @media (min-width: 1024px) { ... }
-      'xl': '1280px', // => @media (min-width: 1280px) { ... }
-    }
-  },
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
+    theme: {
+        extend: {
+            colors: {
+                'accent-1': '#FAFAFA',
+                'accent-2': '#EAEAEA',
+                'accent-7': '#333',
+                success: '#0070f3',
+                cyan: '#79FFE1',
+            },
+            spacing: {
+                28: '7rem',
+            },
+            letterSpacing: {
+                tighter: '-.04em',
+            },
+            lineHeight: {
+                tight: 1.2,
+            },
+            fontSize: {
+                '5xl': '2.5rem',
+                '6xl': '2.75rem',
+                '7xl': '4.5rem',
+                '8xl': '6.25rem',
+            },
+            boxShadow: {
+                small: '0 5px 10px rgba(0, 0, 0, 0.12)',
+                medium: '0 8px 30px rgba(0, 0, 0, 0.12)',
+            },
+        },
+        screens: {
+            'sm': '640px', // => @media (min-width: 640px) { ... }
+            'md': '768px', // => @media (min-width: 768px) { ... }
+            'lg': '1024px', // => @media (min-width: 1024px) { ... }
+            'xl': '1280px', // => @media (min-width: 1280px) { ... }
+        }
+    },
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+    },
 }
